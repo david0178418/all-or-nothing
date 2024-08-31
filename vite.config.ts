d6628cb7f2287563f438c7ecf2e9bf0726a6749d
David Granado <davidjgranado@gmail.com>
2024-08-31 14:30:49 -0500

    Update orientation

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
 			registerType: 'autoUpdate',
 			manifest: {
 				name: 'All or Nothing',
+				orientation: 'portrait',
 				short_name: 'All|Nothing',
 				id: '/?source=pwa',
 				start_url: '/?source=pwa',
