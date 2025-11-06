ab6a7b1e60d5dfff2c97a936b23e5ac471b9a8da
David Granado <davidjgranado@gmail.com>
2025-11-05 21:14:25 -0600

    Remove unused code

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,5 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from 'react'
-import debounce from 'lodash.debounce';
+import { useEffect, useRef, useState } from 'react'
 import useSound from 'use-sound';
 import { useIsSoundEnabled } from './atoms';
 import soundfx from './soundfx.mp3';
@@ -91,49 +90,6 @@ export type DebouncedState<T extends (...args: any) => ReturnType<T>> = ((
 	...args: Parameters<T>
 ) => ReturnType<T> | undefined) &
 	ControlFunctions
-
-export function useDebounceCallback<T extends (...args: any) => ReturnType<T>>(
-	func: T,
-	delay = 500,
-	options?: DebounceOptions,
-): DebouncedState<T> {
-	const debouncedFunc = useRef<ReturnType<typeof debounce>>()
-
-	useUnmount(() => {
-		if (debouncedFunc.current) {
-			debouncedFunc.current.cancel()
-		}
-	});
-
-	const debounced = useMemo(() => {
-	const debouncedFuncInstance = debounce(func, delay, options)
-
-	const wrappedFunc: DebouncedState<T> = (...args: Parameters<T>) => {
-		return debouncedFuncInstance(...args)
-	}
-
-	wrappedFunc.cancel = () => {
-		debouncedFuncInstance.cancel()
-	}
-
-	wrappedFunc.isPending = () => {
-		return !!debouncedFunc.current
-	}
-
-	wrappedFunc.flush = () => {
-		return debouncedFuncInstance.flush()
-	}
-
-	return wrappedFunc
-	}, [func, delay, options])
-
-	// Update the debounced function ref whenever func, wait, or options change
-	useEffect(() => {
-		debouncedFunc.current = debounce(func, delay, options)
-	}, [func, delay, options])
-
-	return debounced;
-}
 
 export function useUnmount(func: () => void) {
 	const funcRef = useRef(func)
