989098df5d392ea450a95d5ce2d7c66ae34a4a2a
David Granado <davidjgranado@gmail.com>
2025-11-15 16:55:31 -0600

    Add keyboard controls to help dialog

--- src/components/help-dialog-trigger.tsx
+++ src/components/help-dialog-trigger.tsx
@@ -1,5 +1,5 @@
 import InfoIcon from "@mui/icons-material/InfoOutlined";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import HelpContent from "./help-content";
 import { useFocusable } from "@/focus/useFocusable";
 import FocusIndicator from "./focus-indicator";
@@ -13,6 +13,11 @@ import {
 	useTheme,
 	Box,
 } from "@mui/material";
+import { useActiveController } from "@/atoms";
+import { useScrollable } from "@/focus/useScrollable";
+import { useBackAction } from "@/input/useBackAction";
+import { InputAction } from "@/input/input-types";
+import { ButtonPromptsBar } from "@/components/button-prompts";
 
 export default
 function HelpDialogTrigger({
@@ -27,12 +32,21 @@ function HelpDialogTrigger({
 	const [open, setOpen] = useState(false);
 	const theme = useTheme();
 	const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+	const activeController = useActiveController();
+	const contentRef = useRef<HTMLDivElement>(null);
 	const { ref, isFocused } = useFocusable({
 		id,
 		group: 'pause-dialog',
 		order,
 		autoFocus,
 		onSelect: () => setOpen(true),
+	});
+
+	useScrollable({ ref: contentRef });
+	useBackAction(() => {
+		if (open) {
+			setOpen(false);
+		}
 	});
 
 	return (
@@ -55,13 +69,23 @@ function HelpDialogTrigger({
 				<DialogTitle id="responsive-dialog-title">
 					How to Play
 				</DialogTitle>
-				<DialogContent>
+				<DialogContent ref={contentRef}>
 					<HelpContent />
 				</DialogContent>
 				<DialogActions>
-					<Button onClick={() => setOpen(false)}>
-						Close
-					</Button>
+					{activeController && (
+						<ButtonPromptsBar
+							controllerType={activeController}
+							prompts={[
+								{ action: InputAction.BACK, label: 'Close' },
+							]}
+						/>
+					)}
+					{!activeController && (
+						<Button onClick={() => setOpen(false)}>
+							Close
+						</Button>
+					)}
 				</DialogActions>
 			</Dialog>
 		</>
