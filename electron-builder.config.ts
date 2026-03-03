516e4784ac48b71a9bf5063fac5ff68e9547219c
David Granado <davidjgranado@gmail.com>
2026-03-02 22:54:42 -0600

    Slim electron

--- electron-builder.config.ts
+++ electron-builder.config.ts
@@ -1,4 +1,12 @@
 import type { Configuration } from 'electron-builder';
+import { rm } from 'fs/promises';
+import { join } from 'path';
+
+const steamDirsToRemove: Readonly<Record<'linux' | 'win32' | 'darwin', readonly string[]>> = {
+	linux: ['osx', 'win64'],
+	win32: ['linux64', 'osx'],
+	darwin: ['linux64', 'win64'],
+} as const;
 
 const config: Configuration = {
 	appId: 'app.allornothing.game',
@@ -11,6 +19,7 @@ const config: Configuration = {
 		'dist-electron/**/*',
 		'!steam_appid.txt',
 	],
+	electronLanguages: ['en-US'],
 	linux: {
 		target: 'AppImage',
 		category: 'Game',
@@ -21,6 +30,20 @@ const config: Configuration = {
 	publish: null,
 	extraMetadata: {
 		main: 'dist-electron/main.js',
+	},
+	afterPack: async function removeUnneededFiles(context) {
+		const { appOutDir, electronPlatformName } = context;
+
+		const dirsToRemove = steamDirsToRemove[electronPlatformName] ?? [];
+		const steamBase = join(
+			appOutDir, 'resources', 'app.asar.unpacked',
+			'node_modules', 'steamworks.js', 'dist',
+		);
+
+		await Promise.all([
+			rm(join(appOutDir, 'LICENSES.chromium.html'), { force: true }),
+			...dirsToRemove.map(dir => rm(join(steamBase, dir), { recursive: true, force: true })),
+		]);
 	},
 };
 
